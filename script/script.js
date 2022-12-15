b1edb30d4f4294e278444018c25ea5c02879afd7
benjamin337rpm <102373732+benjamin337rpm@users.noreply.github.com>
2022-12-15 11:27:55 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,9 @@
+function imgSlider (anything){
+    window.document.querySelector('.pepsi').scr = anything;
+
+}
+
+function changeBgColor(color){
+    const sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
